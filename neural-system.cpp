22a3c20fa2599288a30f0427efed494c5d769c13
Yutaka <p7dea87c7e4n7j@gmail.com>
2016-10-23 17:41:08 +0900

    system version1 is completed maybe.

--- neural-system.cpp
+++ neural-system.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <iostream>
 #include <cmath>
-#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
-double dt = 0.001;
-int numneuron = 2;
+const double dt = 0.001;
+const int numneuron = 2;
 
 /*
 d(potential)/dt = transinput(input)+damp()
@@ -19,18 +20,19 @@ private:
   double potential[numneuron];
 public:
   Neurons(){
-      potential = 2.0*(rand() % 1000);
+    for(int i = 0;i < numneuron;i++)
+      potential[i] = 2.0*(rand() % 1000)/1000;
   }
-  double getpotential() const{return potential[]};
-  double getpotential(int i) const{return potential[i]}
-  double setpotential(int i,double x) {potential[i] = x};
+  double const *getpotential() const{return &(potential[0]);}
+  double getpotential(int i) const{return potential[i];}
+  double setpotential(int i,double x) {return potential[i] = x;}
 };
 
-double differentialEquation(double potential[],int cellnumber){
+double differentialEquation(double const potential[],int cellnumber){
   double otherCellSum = -1*(potential[cellnumber]);
   for(int i = 0;i < numneuron;i++)
     otherCellSum += potential[i];
-  double dpdt = 1/(1+exp(potential[otherCellSum]))-potential[cellnumber];    
+  double dpdt = 1/(1+exp(otherCellSum))-potential[cellnumber];    
   return dpdt;
 }
 
@@ -47,36 +49,42 @@ void timeevolution(Neurons& neuron){/*class is avalable for argument.*/
   double k1[numneuron],k2[numneuron],k3[numneuron],k4[numneuron];
   double temporaryPotential[numneuron];
   for(int i = 0;i < numneuron;i++){
-    k1[i] = differentialEquation(Neurons.getpotential(),i);
-    temporaryPotential[i] = Neurons.getpotential(i) + (1/2)*dt*k1[i];
+    k1[i] = differentialEquation(neuron.getpotential(),i);
+    temporaryPotential[i] = neuron.getpotential(i) + (1/2)*dt*k1[i];
   }
   for(int i = 0;i < numneuron;i++){
-    k2[i] = differentialEquation(temporaryPotential[],i);
-    temporaryPotential[i] = Neurons.getpotential(i) + (1/2)*dt*k2[i];
+    k2[i] = differentialEquation(temporaryPotential,i);
+    temporaryPotential[i] = neuron.getpotential(i) + (1/2)*dt*k2[i];
   }
   for(int i = 0;i < numneuron;i++){
-    k3[i] = differentialEquation(temporaryPotential[],i);
-    temporaryPotential[i] = Neurons.getpotential(i) + dt*k3[i];
+    k3[i] = differentialEquation(temporaryPotential,i);
+    temporaryPotential[i] = neuron.getpotential(i) + dt*k3[i];
   }
   for(int i = 0;i < numneuron;i++){
-    k4[i] = differentialEquation(temporaryPotential[],i);
-    setpotential(i,getpotential(i) + (k1[i]+2*k2[i]+2*k3[i]+k4[i])/6);
+    k4[i] = differentialEquation(temporaryPotential,i);
+    neuron.setpotential(i,neuron.getpotential(i) + (k1[i]+2*k2[i]+2*k3[i]+k4[i])/6);
   }
 }
 
-void dataOutPut(Neurons neuron){
-  for(i = 0;i < numneuron;i++)
-    test0.dat << getpotential(i) << " ";
-  test0.dat << "\n"
-}
+/*void dataOutPut(Neurons& neuron){
+  for(int i = 0;i < numneuron;i++)
+    fout << neuron.getpotential(i) << " ";
+  fout << "\n";
+  }*/
 
-int main{
-  ofstream fout("test0.dat")
+int main(){
   Neurons neuron;
-  dataOutPut(neuron)
-  for(i = 0;i*dt < 100;i++){
+  ofstream fout("test0.dat");
+  fout << 0.0 << " ";
+  for(int i = 0;i < numneuron;i++)
+    fout << neuron.getpotential(i) << " ";
+  fout << "\n";
+  for(int i = 0;i*dt < 100;i++){
     timeevolution(neuron);
-    dataOutPut(neuron)
+    fout << dt*i << " ";
+    for(int j = 0;j < numneuron;j++)
+      fout << neuron.getpotential(j) << " ";
+    fout << "\n";
   }
   return 0;
 }
