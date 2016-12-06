42783b23005fce2d1ac381aa8df5e7f6b5e85588
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2016-12-06 18:55:31 +0900

    edit main.cpp for new class NeuralNetwork.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 #include <cmath>
 #include <stdlib.h>
 #include <vector>
+#include <array>
 #include "declareNeuralSystem.h"
 
 int main(){
+  int NumOfNeuron = 2;
   NeuronTypeA<double> neuronA;
+  NeuronTypeB<double> neuronB;
+  std::array<std::array<double, NumOfNeuron>, NumOfNeuron> ConnectionMatrix;
+  NeuralNetwork = {neuronA,neuronB};
   double dt = 0.0001;//タイムステップ
   double totaltime = 1000.0;//総シミュレーション時間
   std::ofstream fout("test0.dat");
